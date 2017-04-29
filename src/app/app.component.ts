e63eded2aa39518eb62dac88492210489f9337ec
Ignacio Mieites <ia.mieites@gmail.com>
2017-04-29 15:36:55 -0300

    audio funcionando (falta componentizar)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SoundcardComponent } from './components/soundcard/soundcard.component'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SoundBoard';
+
+  ngOnInit(){
+    let audio = new Audio('assets/audio/western.mp3');
+    audio.play();
+  }
+
 }
